b538a51394f74d7fc7dfc0f45fae7672e8e37503
mohesan <mohesan.sreekuladevan@cyclicarx.com>
2019-10-10 19:40:49 -0400

    version 2 using Tone

--- synth.js
+++ synth.js
@@ -1,21 +1,18 @@
 import AudioKeys from "audiokeys";
+import Tone from "tone";
+
+//create a synth and connect it to the master output (your speakers)
+var synth = new Tone.Synth().toMaster();
 
 var keyboard = new AudioKeys();
 let ctx = new AudioContext();
 
-let oscMap = {
-};
-
 keyboard.down(function(note) {
-	let osc = ctx.createOscillator();
-	osc.frequency.value = note.frequency;
-	osc.connect(ctx.destination);
-	osc.start();
-	oscMap[note.frequency]=osc;
+	//play a middle 'C' for the duration of an 8th note
+	synth.triggerAttackRelease('C4', '8n');
+
 	console.log("down",note)
 });
 
 keyboard.up(function(note) {
-	let osc = oscMap[note.frequency];
-	osc.stop();
 });
